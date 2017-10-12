2b498d3142337791fdfe7f2dada06863e38061c2
Rick Copeland <rick@arborian.com>
2017-10-12 12:43:45 -0700

    Class updates, docker shim
    Signed-off-by: Rick Copeland <rick@arborian.com>

---
+++ bin/docker-ansible.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+IMAGE=rick446/ansible
+COMMAND=$(basename $0)
+
+echo "COMMAND" "$COMMAND" "$@"
+
+exec docker run \
+  --interactive --tty --rm \
+  --volume "$PWD":/wd \
+  --workdir /wd \
+  "$IMAGE" "$COMMAND" $@
