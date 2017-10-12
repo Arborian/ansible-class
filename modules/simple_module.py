81f91e773293ebf2885bc6ebd227986b1fd295a3
Rick Copeland <rick@arborian.com>
2017-10-12 07:04:11 -0700

    wip
    Signed-off-by: Rick Copeland <rick@arborian.com>

---
+++ modules/simple_module.py
@@ -0,0 +1,29 @@
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    module_args = dict(
+        name=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False)
+    )
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+    if module.check_mode:
+        return result
+    result['original_message'] = module.params['name']
+    result['message'] = 'goodbye'
+    if module.params['new']:
+        result['changed'] = True
+    if module.params['name'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    run_module()
